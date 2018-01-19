onstash/AppaJokes

commit 366fbfe7925dd6b602faa4e3be8b71459dd038a4

Adds network type info in mixpanel events (#4)

src/utils/network.js
@@ -1,0 +1,15 @@
+import { NetInfo } from "react-native";
+
+export const fetchConnectionInfo = () => {
+  return NetInfo.getConnectionInfo().then(
+    ({ type: connectionType, effectiveConnectionType }) => {
+      if (connectionType === "unknown" || connectionType === "none") {
+        return { connectionType: "UNKNOWN" };
+      }
+      if (connectionType === "wifi") {
+        return { connectionType: connectionType.toUpperCase() };
+      }
+      return { connectionType: effectiveConnectionType.toUpperCase() };
+    }
+  );
+};

src/components/jokes/index.js
@@ -10,6 +10,7 @@ import { fetchJoke, fetchTwoJokes } from "../../data";
 import Timer from "../../utils/timer";
 import GoogleAnalytics from "../../utils/google-analytics";
 import Mixpanel from "../../utils/mixpanel";
+import { fetchConnectionInfo } from "../../utils/network";
 
 class Jokes extends React.Component {
   constructor() {
@@ -50,7 +51,9 @@ class Jokes extends React.Component {
     const { id: jokeID, joke: jokeText } = jokes[previousIndex];
     GoogleAnalytics.trackTimeSpentOnJoke(jokeID, jokeText, timeSpent);
     GoogleAnalytics.trackJokeSwiped(jokeID, jokeText);
-    Mixpanel.trackJokeSwiped(jokeID, jokeText, timeSpent);
+    fetchConnectionInfo().then(connectionInfo => {
+      Mixpanel.trackJokeSwiped(jokeID, jokeText, timeSpent, connectionInfo);
+    });
 
     if (this.state.currentIndex > currentIndex) {
       this.setState(() => ({ currentIndex }));

src/components/main/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { NetInfo } from "react-native";
 
 import Onboarding from "../onboarding";
 import Jokes from "../jokes";
@@ -6,6 +7,7 @@ import Jokes from "../jokes";
 import Cache from "../../data/cache";
 
 import Mixpanel from "../../utils/mixpanel";
+import { fetchConnectionInfo } from "../../utils/network";
 
 class Main extends React.Component {
   constructor() {
@@ -14,14 +16,18 @@ class Main extends React.Component {
   }
 
   componentWillMount() {
-    Mixpanel.trackAppOpened();
+    fetchConnectionInfo().then(connectionInfo => {
+      Mixpanel.trackAppOpened(connectionInfo);
+    });
     Cache.get(Cache.keys.ONBOARDING).then(onboardingShown => {
       this.setState(() => ({ onboardingShown }));
     });
   }
 
   closeOnboarding(jokes) {
-    Mixpanel.trackOnboardingCompleted();
+    fetchConnectionInfo().then(connectionInfo => {
+      Mixpanel.trackOnboardingCompleted(connectionInfo);
+    });
     this.setState(() => ({ onboardingShown: true, jokes }));
   }
 

src/data/cache.js
@@ -2,7 +2,15 @@ import { AsyncStorage } from "react-native";
 
 const get = key => {
   return AsyncStorage.getItem(key).then(response => {
-    return response === null ? false : response;
+    if (response === null) {
+      return false;
+    }
+    try {
+      return JSON.parse(response);
+    } catch (error) {
+      console.log(error);
+      return response;
+    }
   });
 };
 

src/utils/mixpanel.js
@@ -24,6 +24,7 @@ const track = (event, properties = {}) => {
       event,
       properties: Object.assign(properties, {
         token: config.mixpanel.token,
+        distinct_id: distinctID,
         distinctID
       })
     }).catch(e => console.log(e, event));
@@ -59,8 +60,9 @@ const fetchDistinctID = () => {
 };
 
 export default {
-  trackJokeSwiped: (jokeID, text, timeSpent) =>
-    track("Joke Swiped", { jokeID, text, timeSpent }),
-  trackOnboardingCompleted: () => track("Onboarding Completed"),
-  trackAppOpened: () => track("App Opened")
+  trackJokeSwiped: (jokeID, text, timeSpent, connectionInfo) =>
+    track("Joke Swiped", { jokeID, text, timeSpent, ...connectionInfo }),
+  trackOnboardingCompleted: connectionInfo =>
+    track("Onboarding Completed", { ...connectionInfo }),
+  trackAppOpened: connectionInfo => track("App Opened", { ...connectionInfo })
 };

commit 834031227e71edf5e051527dad64779d56f728bf

Fixes flow to determine connectionType (#7)
Fixes bug introduced in https://github.com/onstash/AppaJokes/pull/4.

src/utils/network.js
@@ -2,14 +2,20 @@ import { NetInfo } from "react-native";
 
 export const fetchConnectionInfo = () => {
   return NetInfo.getConnectionInfo().then(
-    ({ type: connectionType, effectiveConnectionType }) => {
+    ({ type: connectionType, effectiveType: effectiveConnectionType }) => {
       if (connectionType === "unknown" || connectionType === "none") {
         return { connectionType: "UNKNOWN" };
       }
       if (connectionType === "wifi") {
         return { connectionType: connectionType.toUpperCase() };
       }
-      return { connectionType: effectiveConnectionType.toUpperCase() };
+      if (
+        effectiveConnectionType &&
+        typeof effectiveConnectionType === "string"
+      ) {
+        return { connectionType: effectiveConnectionType.toUpperCase() };
+      }
+      return { connectionType: "ERROR" };
     }
   );
 };