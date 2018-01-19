e3bf666e1135b8160635978aa3865a7c18293f13
Santosh Venkatraman <santosh.venk@gmail.com>
2018-01-19 12:50:46 +0530

    Cleans up Onboarding view
    * Fixes onboarding copy
    * Adds animation for better UX

--- src/components/onboarding/index.js
+++ src/components/onboarding/index.js
@@ -1,5 +1,12 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableWithoutFeedback,
+  Animated,
+  Easing
+} from "react-native";
 
 import Swiper from "react-native-swiper";
 
@@ -9,11 +16,22 @@ import Cache from "../../data/cache";
 import GoogleAnalytics from "../../utils/google-analytics";
 
 class Onboarding extends React.Component {
-  state = {
-    jokes: null
-  };
+  constructor() {
+    super();
+    this.state = {
+      jokes: null
+    };
+    this.handlePressIn = this.handlePressIn.bind(this);
+    this.handlePressOut = this.handlePressOut.bind(this);
+  }
 
   componentWillMount() {
+    // Left of screen
+    this._textPositionX = new Animated.Value(100);
+    this._buttonScale = new Animated.Value(1);
+    // Below the screen
+    this._buttonPositionY = new Animated.Value(100);
+
     GoogleAnalytics.trackScreenName("Onboarding");
     this.mounted = true;
     Cache.set(Cache.keys.ONBOARDING, true);
@@ -24,51 +42,104 @@ class Onboarding extends React.Component {
     });
   }
 
+  componentDidMount() {
+    Animated.sequence([
+      Animated.timing(this._textPositionX, {
+        toValue: 0,
+        duration: 275,
+        useNativeDriver: true,
+        easing: Easing.elastic()
+      }),
+      Animated.timing(this._buttonPositionY, {
+        toValue: 0,
+        duration: 275,
+        useNativeDriver: true,
+        easing: Easing.linear()
+      })
+    ]).start();
+  }
+
   componentWillUnmount() {
     this.mounted = false;
   }
 
+  handlePressIn() {
+    Animated.spring(this._buttonScale, {
+      toValue: 0.75,
+      useNativeDriver: true
+    }).start();
+  }
+
+  handlePressOut() {
+    Animated.spring(this._buttonScale, {
+      toValue: 1,
+      friction: 5,
+      tension: 40,
+      useNativeDriver: true
+    }).start(() => {
+      this.props.closeOnboarding(this.state.jokes);
+    });
+  }
+
   render() {
+    const animatedTextContentStyle = {
+      transform: [
+        {
+          translateX: this._textPositionX
+        }
+      ]
+    };
+    const animatedButtonStyle = {
+      transform: [
+        {
+          scale: this._buttonScale
+        },
+        {
+          translateY: this._buttonPositionY
+        }
+      ]
+    };
     return (
-      <Swiper showsPagination={true}>
-        <View style={styles.screenOne}>
-          <Text style={styles.screenOneHeader}>Welcome to AppaJokes, kid!</Text>
-          <Text style={styles.screenOneText}>
-            {
-              "Discover and share Appa a.k.a Dad jokes. \nSwipe right to continue."
-            }
-          </Text>
+      <View style={styles.screen}>
+        <View style={styles.textContainer}>
+          <Text style={styles.header}>Welcome to AppaJokes!</Text>
+          <Animated.View style={animatedTextContentStyle}>
+            <Text style={styles.text}>
+              Swipe your way to glorious Dad jokes!
+            </Text>
+          </Animated.View>
         </View>
-        <View style={styles.screenTwo}>
-          <Text style={styles.screenOneText}>
-            Noice. You can read more jokes by swiping right.
-          </Text>
-          <Text
-            style={styles.screenTwoButton}
-            onPress={() => this.props.closeOnboarding(this.state.jokes)}
-          >
-            Beam me to the jokes, Scotty!
-          </Text>
-        </View>
-      </Swiper>
+        <TouchableWithoutFeedback
+          onPressIn={this.handlePressIn}
+          onPressOut={this.handlePressOut}
+        >
+          <Animated.View style={[styles.button, animatedButtonStyle]}>
+            <Text style={styles.buttonText}>Get started</Text>
+          </Animated.View>
+        </TouchableWithoutFeedback>
+      </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  screenOne: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "white",
-    padding: 30
+  screen: {
+    flex: 2,
+    backgroundColor: "#F5F5F5"
   },
-  screenOneHeader: {
+  textContainer: {
+    flex: 2,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  header: {
     fontSize: 19,
     fontWeight: "bold",
-    color: "#333"
+    color: "#333",
+    padding: 30
   },
-  screenOneText: {
+  text: {
+    padding: 30,
     marginTop: 20,
     fontSize: 16,
     fontWeight: "400",
@@ -76,19 +147,17 @@ const styles = StyleSheet.create({
     textAlign: "center",
     lineHeight: 30
   },
-  screenTwo: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "white"
-  },
-  screenTwoButton: {
+  button: {
     marginTop: 20,
+    padding: 15,
+    width: "100%",
+    backgroundColor: "#6A1B9A"
+  },
+  buttonText: {
     fontSize: 17,
-    padding: 10,
-    fontWeight: "400",
-    color: "#666",
-    backgroundColor: "#EEE"
+    fontWeight: "700",
+    color: "white",
+    textAlign: "center"
   }
 });
 
